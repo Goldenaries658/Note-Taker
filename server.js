6ae55aa4fc9821263c1621441da148b7721c4837
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-06-14 16:28:51 +0100

    Created get api method

--- server.js
+++ server.js
@@ -15,6 +15,25 @@ const htmlPath = path.join(__dirname, './public');
 const apiPath = path.join(__dirname, './db');
 const notesDB = path.join(apiPath + '/db.json');
 
+// API Routes
+// Returning notes from db
+app.get('/api/notes', async (req, res) => {
+  try {
+    // Reading notes and saving to variable as a string
+    const notes = fs.readFileSync(notesDB, (err, data) => {
+      if (err) throw new Error(`server.js ln20 - fs.readFile(): ${err}`);
+      return data;
+    });
+
+    // Parsing and returning the notes
+    const notesJSON = JSON.parse(notes);
+    return res.status(200).json(notesJSON);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).send('Error 404: No Notes Found.');
+  }
+});
+
 // HTML Routes
 app.get('/notes', (req, res) => res.sendFile(`${htmlPath}/notes.html`));
 app.get('*', (req, res) => res.sendFile(`${htmlPath}/index.html`));
