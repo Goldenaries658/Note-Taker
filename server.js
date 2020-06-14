f01a11ed4581084c8eeb2c06288a3d33424a718b
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-06-14 16:26:58 +0100

    Created HTML routes

--- server.js
+++ server.js
@@ -9,3 +9,15 @@ const PORT = process.env.PORT || 8080;
 // Parsing data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Defining paths
+const htmlPath = path.join(__dirname, './public');
+const apiPath = path.join(__dirname, './db');
+const notesDB = path.join(apiPath + '/db.json');
+
+// HTML Routes
+app.get('/notes', (req, res) => res.sendFile(`${htmlPath}/notes.html`));
+app.get('*', (req, res) => res.sendFile(`${htmlPath}/index.html`));
+
+// Starting server
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
