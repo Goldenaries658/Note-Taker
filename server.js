a7397e20aba737534de6def88c127987035b4d5b
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-06-14 19:19:47 +0100

    Created notes api delete function

--- server.js
+++ server.js
@@ -40,10 +40,8 @@ app.post('/api/notes', async (req, res, next) => {
     newNote['id'] = notes.length;
     notes.push(newNote);
 
-    // Saving the new note to db
-    console.log('Writing Note...');
-    fs.writeFileSync(notesDB, JSON.stringify(notes, null, 2));
-    console.log('Save Complete!');
+    // Saving the new notes array to db
+    writeNote(notes);
 
     // Returning new note to client
     res.status(200).send(newNote);
@@ -53,9 +51,44 @@ app.post('/api/notes', async (req, res, next) => {
   }
 });
 
+app.delete('/api/notes/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+
+    // Reading existing notes
+    const notes = JSON.parse(await readNotes());
+
+    let removedNote = {};
+    // Looping through notes array
+    for (i in notes) {
+      // Checking id and removing the matching note
+      if (notes[i].id == id) {
+        removedNote = notes.splice(i, 1);
+
+        // Shifting ids down to fill gap
+        for (j = i; j < notes.length; j++) {
+          notes[j].id--;
+        }
+        break;
+      }
+    }
+
+    // Saving the new notes array to db
+    writeNote(notes);
+
+    res.status(200);
+  } catch (err) {
+    console.log(`Error: server.js - app.delete('api/notes'): ${err}`);
+    next(err);
+  }
+});
+
 // HTML Routes
 app.get('/notes', (req, res) => res.sendFile(`${htmlPath}/notes.html`));
 app.get('*', (req, res) => res.sendFile(`${htmlPath}/index.html`));
+
+// Starting server
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 
 // Function to read existing notes
 const readNotes = async () => {
@@ -65,5 +98,8 @@ const readNotes = async () => {
   });
 };
 
-// Starting server
-app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+const writeNote = (notes) => {
+  console.log('Writing Notes to db...');
+  fs.writeFileSync(notesDB, JSON.stringify(notes, null, 2));
+  console.log('Save Complete!');
+};
